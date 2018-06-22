ae76d40d0bec8caef56830a85d2e81377b00725f
VidurWadhwa <vidur2wadhwa@gmail.com>
2018-06-22 23:45:33 +0530

    Code working

---
+++ backtest/backtest.py
@@ -0,0 +1,119 @@
+"""Custom Backtesting Library from Scratch"""
+
+import pandas as pd
+from pandas import to_datetime
+from . import signals
+
+
+class Backtest(object):
+    """Main Backtesting Object"""
+
+    def __init__(self, ohlcv, ini_cash, start=None, end=None, ini_shares=0, algo=None, params=None):
+        """
+        Backtest object constructor:
+        params:
+            algo: Preferred algo (moving_average, rsi, etc)
+        """
+        if start and end:
+            self.start = self.get_closest_stamp(ohlcv.index, to_datetime(start), pd.Timedelta(1, unit='d'), 'start')
+            self.end = self.get_closest_stamp(ohlcv.index, to_datetime(end), pd.Timedelta(1, unit='d'), 'end')
+        else:
+            self.start = ohlcv.index.min()
+            self.end = ohlcv.index.max()
+        self.ohlcv = ohlcv.loc[self.start:self.end].copy()
+        self.ini_cash = ini_cash
+        self.ini_shares = ini_shares
+        self.curr_cash = ini_cash
+        self.curr_shares = ini_shares
+        if algo:
+            if params:
+                self.buy, self.sell = signals.evaluate(self.ohlcv, algo, params)
+            else:
+                self.buy, self.sell = signals.evaluate(self.ohlcv, algo)
+
+    def get_closest_stamp(self, index, timestamp, delta, type):
+        if type == 'start':
+            closest = timestamp
+            while closest not in index:
+                closest += delta
+            return closest
+        elif type == 'end':
+            closest = timestamp
+            while closest not in index:
+                closest -= delta
+            return closest
+
+    def run_backtest(self, number, buy=None, sell=None):
+        """ Run backtest here """
+        self.reset()
+        ohlcv = self.ohlcv
+        ohlcv['buy'] = buy if buy else self.buy
+        ohlcv['sell'] = sell if sell else self.sell
+        trades = []
+        timedelta = pd.Timedelta(1, unit='d')
+        for index, row in ohlcv[(ohlcv['buy'] == True) | (ohlcv['sell'] == True)].iterrows():
+            if index == self.end:
+                continue
+            else:
+                trade = {}
+                trade['timestamp'] = index
+                trade['share_price'] = row['close']
+                if(row['buy']):
+                    self.curr_shares += number
+                    self.curr_cash -= number * (ohlcv.loc[self.handle_index(ohlcv, index, timedelta)]['open'])
+                    trade['owned_shares'] = self.curr_shares
+                    trade['remaining_cash'] = self.curr_cash
+                    trade['type'] = 'buy'
+                elif(row['sell']):
+                    self.curr_shares -= number
+                    self.curr_cash += number * (ohlcv.loc[self.handle_index(ohlcv, index, timedelta)]['open'])
+                    trade['owned_shares'] = self.curr_shares
+                    trade['remaining_cash'] = self.curr_cash
+                    trade['type'] = 'sell'
+                trades.append(trade)
+        trades_df = pd.DataFrame(trades).set_index('timestamp') # Convert to pandas DataFrame
+        return self._summarize(trades_df)
+
+    def _summarize(self, trades):
+        """
+        Summarizes the backtest
+            summary: (Dict with these Params)
+                duration_analyzed
+                number_of_trades
+                simple_return
+                remaining_cash
+                owned_shares
+                portfolio_value
+        """
+        summary = {}
+        ini_portfolio = self.ini_cash + (self.ohlcv.loc[self.start]['open'] * self.ini_shares)
+        final_portfolio = self.curr_cash + (self.ohlcv.loc[self.end]['close'] * self.curr_shares)
+
+        summary['duration_analyzed'] = str(self.end - self.start)
+        summary['number_of_trades'] = len(trades)
+        summary['simple_return'] = str(100 * (final_portfolio - ini_portfolio) / ini_portfolio) + ' %'
+        summary['remaining_cash'] = self.curr_cash
+        summary['owned_shares'] = self.curr_shares
+        summary['portfolio_value'] = final_portfolio
+        self._summary = summary
+        return summary
+
+    def summary(self):
+        """ Access summary """
+        if self._summary:
+            return self._summary
+        raise KeyError('Run the Algorithm first!')
+
+    def reset(self):
+        self.curr_cash = self.ini_cash
+        self.curr_shares = self.ini_shares
+
+    def handle_index(self, df, index, timedelta):
+        index+timedelta
+        while index not in df.index:
+            index = index + timedelta
+        return index
+
+
+
+
--- backtest/views.py
+++ backtest/views.py
@@ -1,19 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-
 from django.http import HttpResponse
-# Create your views here.
-
-#importing loading from django template
 from django.template import loader
-
-from django.views.decorators.csrf import csrf_exempt
-
-#disabling csrf (cross site request forgery)
-@csrf_exempt
+import pandas as pd
+from . import backtest
 
 def index(request):
     template = loader.get_template('form.html')
@@ -21,22 +9,31 @@ def index(request):
 
 
 def submit(request):
-
-    #if post request came
     if request.method == 'GET':
-        #getting values from GET
-        capital = request.GET.get('capital')
-        initialShare=request.GET.get('Initial share')
-        numberOfShare=request.GET.get('Number of share')
-        selectChoice=request.GET.get('select-choice')
+        capital = int(request.GET.get('capital'))
+        initial_share = int(request.GET.get('Initial share'))
+        number_of_share = int(request.GET.get('Number of share'))
+        start_day = request.GET.get('start-day')
+        start_month = request.GET.get('start-month')
+        start_year = request.GET.get('start-year')
+        end_day = request.GET.get('end-day')
+        end_month = request.GET.get('end-month')
+        end_year = request.GET.get('end-year')
 
-
-
-
-        print(capital)
-        print(initialShare)
-        print(numberOfShare)
-        print(selectChoice)
-
+        start_date = start_year+'-'+start_month+'-'+start_day
+        end_date = end_year + '-' + end_month + '-' + end_day
+        ohlcv = pd.read_csv('/Users/vidur/dev/finance/algotbackend/backtest/daily_TSLA.csv'
+                            , parse_dates=['timestamp']).sort_values('timestamp').set_index('timestamp')
+        test = backtest.Backtest(ohlcv,
+                                 capital,
+                                 start_date,
+                                 end_date,
+                                 initial_share,
+                                 algo='moving_average',
+                                 params={
+                                     'swindow': 20,
+                                     'lwindow': 100
+                                 })
+        print(test.run_backtest(number_of_share))
 
     return HttpResponse("submission successful")
